fcbfb7a89a93d8c60d154e612b28ac780f975ab0
angelqtypie <angeljustineballaso07@gmail.com>
2023-11-17 21:38:41 -0800

    added Student(2)

---
+++ src/Student(2)/Student.java
@@ -0,0 +1,31 @@
+package Student(2);
+public class Student {
+    String name;
+    String address;
+    int roll_no;
+    int phone_no;
+
+    void Student1(){
+        name="Sam";
+        address="Cagayan";
+        roll_no=1;
+        phone_no=2218888;
+
+System.out.println("Name: " + name);
+System.out.println("Address: " + address);
+System.out.println("roll Number: " + roll_no);
+System.out.println("Phone Number: " + phone_no);
+    }
+    void Student2(){
+        name="John";
+        address="Manolo Fortich";
+        roll_no=2;
+        phone_no=2227258;
+
+System.out.println("Name: " + name);
+System.out.println("Address: " + address);
+System.out.println("roll Number: " + roll_no);
+System.out.println("Phone Number: " + phone_no);    
+    }
+
+}
