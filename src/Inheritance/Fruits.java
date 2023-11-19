cce8bf376aadff60c024f6b98aca13b43cbd9042
angelqtypie <angeljustineballaso07@gmail.com>
2023-11-18 18:24:16 -0800

    Add Fruits

--- src/Inheritance/Fruits.java
+++ src/Inheritance/Fruits.java
@@ -1,5 +1,16 @@
 package Inheritance;
 
 public class Fruits {
+    String name;
     
+    Fruits(String name){
+    this.name=name;
+    }
+    
+    void eat(){
+    System.out.println(name +" eats the prutas");
+    }
+    void peel(){
+    System.out.println(name +" peeled the prutas");
+    }  
 }
