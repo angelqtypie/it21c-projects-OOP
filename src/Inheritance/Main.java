e69e4e573915204af4514118c3971343e8a1f98d
angelqtypie <angeljustineballaso07@gmail.com>
2023-11-18 18:22:12 -0800

    add Main

---
+++ src/Inheritance/Main.java
@@ -0,0 +1,12 @@
+package Inheritance;
+
+public class Main {
+  public static void main(String[] args){
+  
+      Fruits myFave = new Fruits("Angel");
+      
+      myFave.eat();
+      myFave.peel();
+      
+  }  
+}
